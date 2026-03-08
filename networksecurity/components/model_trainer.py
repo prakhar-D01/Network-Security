2e5cf69e300bd41d1e8809f239bd06f549148d86
prakhar-D01 <prakhardubey110@gmail.com>
2026-03-08 17:20:34 +0530

    Dagshub

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -18,6 +18,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='prakhar-D01', repo_name='Network-Security', mlflow=True)
+
 class ModelTrainer:
     def __init__(self, model_trainer_config:ModelTrainerConfig, 
                  data_transformation_artifact:DataTransformationArtifact):
@@ -64,15 +67,15 @@ class ModelTrainer:
             "Gradient Boosting":{
                 # 'loss':['log_loss', 'exponential'],
                 #'learning_rate':[.1,.01,.05,.001],
-                'subsample':[0.6,0.7,0.75,0.85,0.9],
+                #'subsample':[0.6,0.7,0.75,0.85,0.9],
                 #'criterion':['squared_error', 'friedman_mse'],
                 # 'max_features':['auto','sqrt','log2'],
-                'n_estimators': [8,16,32,64,128,256]
+                'n_estimators': [8,16,32,128,256]
             },
             "Logistic Regression":{},
             "AdaBoost":{
                 #'learning_rate':[.1,.01,.001],
-                'n_estimators': [8,16,32,64,128,256]
+                'n_estimators': [8,16,32,128,256]
             }
         }
 
